fd64bde6818341637437322a3570cb99f9a72788
Angelika Tyborska <angelika@steadyhq.com>
2020-09-22 13:09:37 +0200

    Set up easygettext and vue-gettext

---
+++ src/components/TestComponent1.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 v-translate>Test String 1</h1>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent1'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TestComponent2.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+const TestComponent2 = Vue.component('TestComponent2', {
+  template: `
+    <h1 v-translate>Test String 2</h1>
+  `
+})
+
+export default TestComponent2
